2544bbef7cef081ef2017d388c35766156495f20
Nathaniel Owens <nowens3@student.fitchburgstate.edu>
2023-05-09 11:58:33 -0400

    Enemy Logic Implemented (mostly)
    Still needs some work to get working like a NavMesh and animation but enemies are now at least capable of moving towards the player and even has logic/triggers in place to do damage.

--- Assets/Scripts/nateScripts/EnemyStateMachine.cs
+++ Assets/Scripts/nateScripts/EnemyStateMachine.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStateMachine : MonoBehaviour
 {
+    private Owens_StateMachine brain;
+    private GameObject player;
+
+    public float distanceToChase;
+    public float distanceToAttack;
+    public float cooldownTimer = 1;
+
+    private bool withinAttackRange;
+    private bool playerSpotted;
+
+    private NavMeshAgent nav;
+    private SphereCollider hurtbox;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("PlayerAvatar");
+        nav = GetComponent<NavMeshAgent>();
+        brain = GetComponent<Owens_StateMachine>();
+        hurtbox = GetComponent<SphereCollider>();
+        hurtbox.enabled = false;
+        playerSpotted= false;
+
     }
 
     // Update is called once per frame
@@ -15,4 +35,49 @@ public class EnemyStateMachine : MonoBehaviour
     {
         
     }
+    
+    //~~~~~~~~~~~~~~~STATES~~~~~~~~~~~~~~~~~~~~~
+
+    void emptyStart()
+    {
+        nav.ResetPath();
+    }
+    void empty()
+    {
+        if (playerSpotted)
+        {
+            brain.PushState(Violence, ViolenceEnter, ViolenceExit);
+        }
+    }
+    void emptyExit()
+    {
+
+    }
+
+    void Violence() // This does not work for when we go up ladders
+    {
+        nav.SetDestination(player.transform.position);
+        if (withinAttackRange)
+        {
+            brain.PushState(Attack, AttackEnter, AttackExit);
+        }
+    }
+    void ViolenceEnter(){}
+    void ViolenceExit(){}
+
+    void AttackEnter(){ }
+    void Attack()
+    {
+        //anim.setBool("Attack", true);
+        hurtbox.enabled = true;
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0)
+        {
+            brain.PushState(Violence, ViolenceEnter, ViolenceExit);
+            cooldownTimer = 1;
+        }
+    }
+    void AttackExit() { }
+
+
 }
